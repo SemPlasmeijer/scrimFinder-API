a61de63eb6aa4b3cf5702ca8174ce53c6f86d854
SemPlasmeijer <plasmeijer.sem@gmail.com>
2023-02-28 11:35:34 +0100

    springboot

---
+++ src/main/java/com/scrimfinderapi/scrimfinder/ScrimfinderApplication.java
@@ -0,0 +1,13 @@
+package com.scrimfinderapi.scrimfinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScrimfinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScrimfinderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/scrimfinderapi/scrimfinder/ScrimfinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scrimfinderapi.scrimfinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScrimfinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
