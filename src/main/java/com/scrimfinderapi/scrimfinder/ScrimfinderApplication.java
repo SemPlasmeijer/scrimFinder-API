afef5393e6ddae846b9fe47d933a4b05099daf0f
Sem Plasmeijer <plasmeijer.sem@gmail.com>
2023-02-28 15:32:58 +0100

    quick get

--- src/main/java/com/scrimfinderapi/scrimfinder/ScrimfinderApplication.java
+++ src/main/java/com/scrimfinderapi/scrimfinder/ScrimfinderApplication.java
@@ -2,9 +2,12 @@ package com.scrimfinderapi.scrimfinder;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @RestController
@@ -14,8 +17,18 @@ public class ScrimfinderApplication {
 		SpringApplication.run(ScrimfinderApplication.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/hello").allowedOrigins("http://localhost:5173");
+			}
+		};
+	}
+
 	@GetMapping("/hello")
 	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-		return String.format("Hello %s!", name);
+		return String.format("Hella %s!", name);
 	}
 }
