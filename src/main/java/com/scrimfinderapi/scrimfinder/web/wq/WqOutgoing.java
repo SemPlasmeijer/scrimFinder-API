7ecbd501bfab96cb734f2ff76b49d3e0ba57735a
SemPlasmeijer <plasmeijer.sem@gmail.com>
2023-03-14 12:48:14 +0100

    get On WQ service

--- src/main/java/com/scrimfinderapi/scrimfinder/web/wq/WqOutgoing.java
+++ src/main/java/com/scrimfinderapi/scrimfinder/web/wq/WqOutgoing.java
@@ -3,13 +3,16 @@ package com.scrimfinderapi.scrimfinder.web.wq;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 @RequestMapping("/api/out/wq")
 public class WqOutgoing {
     @GetMapping("/logs")
-    public String getWorkerQueueLogs(){
+    public Object getWorkerQueueLogs(){
+        String uri = "http://localhost:8081/api/hello";
+        RestTemplate restTemplate = new RestTemplate();
 
-        return "lmao";
+        return restTemplate.getForObject(uri, String.class);
     }
 }
